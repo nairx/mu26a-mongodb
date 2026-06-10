eb1a2d7dac1e6c333b3bc7a5437e94d6fec0d2f8
nairx <nairxprv@gmail.com>
2026-06-10 12:36:30 +0530

    updated

--- q6.js
+++ q6.js
@@ -1,4 +1,7 @@
 db.employees.aggregate([
     { $match: { department: "IT" } },
-    { $project: { name: 1, email: 1 } }
+    { $project: { name: 1, email: 1 } },
+    { $sort: { name: 1 } },
+    { $limit: 3 },
+    { $skip: 1 }
 ])
